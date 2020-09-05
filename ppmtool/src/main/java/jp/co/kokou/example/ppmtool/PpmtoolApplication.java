4bd1a48c00e9b487ee87cc8de50e3e0a47771643
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2020-09-05 13:02:33 +0900

    Create Spring Project for Server-side.

---
+++ ppmtool/src/main/java/jp/co/kokou/example/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package jp.co.kokou.example.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
